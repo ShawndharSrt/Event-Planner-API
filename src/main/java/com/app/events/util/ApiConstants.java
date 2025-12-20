b1ecd4590e9ba1202b56e3a520311eb9dd5121bb
Shawndhar <b.p.shawndharsrt10@gmail.com>
2025-12-20 20:20:44 +0530

    1 day expiration token

--- src/main/java/com/app/events/util/ApiConstants.java
+++ src/main/java/com/app/events/util/ApiConstants.java
@@ -2,5 +2,5 @@ package com.app.events.util;
 
 public class ApiConstants {
 
-    public static final long EXPIRATION_TIME = 172800000;
+    public static final long EXPIRATION_TIME = 86400000; // 1 day
 }
